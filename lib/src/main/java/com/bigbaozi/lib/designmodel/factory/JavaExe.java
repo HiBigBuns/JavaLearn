3fcc882a606fb14105de6ddbf4bbe57c0be31f0c
fenglin.zhang <fenglin.zhang@haitaichina.com>
2018-05-24 16:38:33 +0800

    工厂模式 描述

--- lib/src/main/java/com/bigbaozi/lib/designmodel/factory/JavaExe.java
+++ lib/src/main/java/com/bigbaozi/lib/designmodel/factory/JavaExe.java
@@ -6,6 +6,9 @@ package com.bigbaozi.lib.designmodel.factory;
  * Email: zhfenglin@163.com
  * Comment: // 工厂模式 测试类
  *
+ *  在面向对象编程中, 最通常的方法是一个new操作符产生一个对象实例,new操作符就是用来构造对象实例的。但是在一些情况下, new操作符直接生成对象会带来一些问题。
+ *
+ *
  *   如何能轻松方便地构造对象实例，而不必关心构造对象实例的细节和复杂过程。
  *
  *                                          -------使用工厂模式
