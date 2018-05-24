378d09792da6f74123c11210df3d03db1a1da66f
fenglin.zhang <fenglin.zhang@haitaichina.com>
2018-05-24 12:10:02 +0800

    Initial commit

---
+++ app/src/main/java/com/bigbaozi/javalearn/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bigbaozi.javalearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ lib/src/main/java/com/bigbaozi/lib/MyClass.java
@@ -0,0 +1,11 @@
+package com.bigbaozi.lib;
+
+/**
+ *  Java Learn  eg: designModel   
+ */
+
+public class MyClass {
+    public static void main(String[] args){
+      System.out.print("JavaModel in AndroidStudio");
+    }
+}
