e3eef19d1078197301b1ec14014aa2d5a47cae9f
fenglin.zhang <fenglin.zhang@haitaichina.com>
2018-05-24 13:50:31 +0800

    工厂设计模式

---
+++ lib/src/main/java/com/bigbaozi/lib/designmodel/factory/IFactory.java
@@ -0,0 +1,13 @@
+package com.bigbaozi.lib.designmodel.factory;
+
+/**
+ * Name: IFactory
+ * Author: zhangfenglin
+ * Email: zhfenglin@163.com
+ * Comment: //TODO
+ * Date: 2018-05-24 13:45
+ */
+public interface IFactory {
+
+    void  operate();
+}
---
+++ lib/src/main/java/com/bigbaozi/lib/designmodel/factory/JavaExe.java
@@ -0,0 +1,11 @@
+package com.bigbaozi.lib.designmodel.factory;
+
+/**
+ * Name: JavaExe
+ * Author: zhangfenglin
+ * Email: zhfenglin@163.com
+ * Comment: //TODO
+ * Date: 2018-05-24 13:43
+ */
+public class JavaExe {
+}
